4d190a67412954eedc7fb4a2633c36140097e892
Nataliya <nataliyasjmonova@gmail.com>
2022-08-14 19:48:38 +0300

    Footer

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -5,9 +5,12 @@ import { useRouter } from 'next/router';
 import { topics } from '../utils/constants';
 
 const Discover = (props) => {
-    const activeTopicStyle = "";
-    const topicStyle = "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 rounded xl:rounded-full flex" +
+    const router = useRouter();
+    const { topic } = router.query;
+    const topicStyle = "xl:border-2 hover:bg-primary  xl:border-gray-300 px-3 rounded xl:rounded-full flex" +
         " items-center gap-2 justify-center cursor-pointer text-black";
+    const activeTopicStyle = "xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 rounded xl:rounded-full flex" +
+        " items-center gap-2 justify-center cursor-pointer text-[#F51997]";
    return  (
     <div className={'xl:border-b-2 xl:border-gray-200 pb-6'}>
        <p className={'text-gray-500 font-semibold m-3 mt-4 hidden xl:block'}>
@@ -17,7 +20,9 @@ const Discover = (props) => {
             {topics.map((item) => (
                 <Link href={`/?topic=${item.name}`}
                 key={item.name}>
-                    <div className={topicStyle}>
+                    <div className={topic === item.name ?
+                        activeTopicStyle : topicStyle}
+                    >
                         <span className={'font-bold text-2xl xl:text-md'}>
                         {item.icon}
                         </span>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import  { footerList1, footerList2, footerList3 } from '../utils/constants';
 
-const Footer = (props) => (
-    <div>
-    Footer
+const List = ({ items, mt }: { items: string[], mt:boolean}) => (
+    <div className={`flex flex-wrap gap-2 ${mt && 'mt-5'}`}>
+        {items.map((item) => (
+            <p key={item} className={'text-gray-400 tex-sm cursor-pointer hover:underline'}>
+                {item}
+            </p>
+        ))}
+    </div>
+)
+const Footer = () => (
+    <div className={'mt-6 hidden xl:block'}>
+        <List items={footerList1} mt={false}/>
+        <List items={footerList2} mt/>
+        <List items={footerList3} mt/>
+        <p clssName={'text-gray-400 text-sm mt-5'}>2002 TikTi</p>
     </div>
 );
 
